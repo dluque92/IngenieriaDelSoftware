2cb801a7c500104812bec5049056de42825f000f
dluque92 <david_92.8@hotmail.com>
2015-05-29 20:09:02 +0200

    Practica Beta.01

--- Junit/src/Prueba.java
+++ Junit/src/Prueba.java
@@ -1,12 +1,27 @@
 import static org.junit.Assert.*;
-import org.junit.Test;
 
+import org.junit.Before;
+import org.junit.Test;
 
 public class Prueba {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	private BinaryString bitset1, bitset2, 
+							bitset3, bitset4;
+	private String pruebaString1 = "1010",
+					pruebaString2 = "101010",
+					pruebaString3 = "11111";
+	
+	@Before
+	public void beforeTest(){
+		bitset1 = new BinaryString(pruebaString1);
+		bitset2 = new BinaryString(pruebaString2);
+		bitset3 = new BinaryString(pruebaString3);
+		bitset4 = new BinaryString(10000);
 	}
-
+	
+	@Test
+	public void testLongitud(){
+		assertEquals(4, bitset1.length());
+		assertEquals(10000, bitset4.length());
+	}
 }
